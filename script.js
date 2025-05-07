887184f6a043a73a96063f037b02e0cf14db0057
Patience <123624569+Patienceedward@users.noreply.github.com>
2025-05-07 15:11:22 +0000

    Complete Election Results

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
-// Get the 2D rendering context of the canvas element with ID 'electionChartPercentage'
-const ctx = document.getElementById('electionChartPercentage').getContext('2d');
+// My javascript file for the pie chart. I tried to make it as simple as possible. making sure to use the right names and variables
 
-// Create a new Chart instance using Chart.js
-const electionChartPercentage = new Chart(ctx, {
-  type: 'pie', // Define chart type as pie
+
+const canvas = document.getElementById('electionChartPercentage');
+// I made the canvascontext 2d to make the pie chart look 2D, I think it looks better this way.
+const ctx = canvas.getContext('2d');
+
+//I hardcodied the resluts and data for the pie chart//
+//TODO: learn how to get this data from a database or even input and displsay
+ const partynames = ['ABC (42%)', 'BA (10%)', 'ACTIONPA (27%)', 'PFF (21%)'];
+const votePercentages = [42, 10, 27, 21];
+const colors = ['#FF1744', '#5CAF50', '#00E5FF', '#FFFF00'];
+
+
+const electionChart = new Chart(ctx, {
+  type: 'pie', 
+
   data: {
-    // Labels for each slice of the pie chart
-    labels: ['DAD (45%)', 'PPP (5%)', 'CCCC (27%)', 'MCD (23%)'],
+    labels: partynames, //I trired to change current party name
     datasets: [{
-      // Data values corresponding to each label
-      data: [45, 5, 27, 23],
-      // Colors for each pie slice
-      backgroundColor: ['#FF1744', '#5CAF50', '#00E5FF', '#FFFF00'],
-      // Border color for pie slices
+      data: votePercentages,
+      backgroundColor: colors,
       borderColor: '#fff',
-      borderWidth: 2 // Width of border between slices
+      borderWidth: 8
     }]
+
+    
   },
   options: {
-    responsive: true, // Make the chart responsive to screen size
+    responsive: true, //This makes it look good on all devices. These are just add on, to give it a bit more personiality and honestyly see else i could do. just to challenge myself a bit more
     plugins: {
-      // Configure the legend
       legend: {
-        position: 'bottom', // Position legend below the chart
+        position: 'bottom', 
         labels: {
-          color: '#333', // Legend text color
+          color: '#333', 
           font: {
-            size: 14 // Legend text size
+            size: 12
           }
         }
       },
-      // Add and configure the chart title
-      title: {
-        display: true, // Display the title
-        text: 'Votes Share by Party', // Title text
+
+      
+      title: { //This is the title of the pie chart. Made it bold and big so it stands out
+        display: true, 
+        text: 'Votes Share by Party', 
         font: {
-          size: 18 // Title font size
-        }
+          size: 10,
+          weight: 'bold'
+        },
+        color: '#333'
+      
       }
     }
   }
